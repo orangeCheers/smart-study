5d0d7cf99c79b3b0310e0bfcdb438ff33df84112
orangeCheers <chengqingkai@ysten.com>
2026-04-20 18:06:50 +0800

    Updates

---
+++ assets/tesseract-B4qsg-su.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index-BzRg5XYC.js","assets/useQuestions-DJAATgou.js"])))=>i.map(i=>d[i]);
+import{_ as r}from"./index-BO7f5XRq.js";async function o(t){return(await(await r(()=>import("./index-BzRg5XYC.js").then(e=>e.i),__vite__mapDeps([0,1]))).recognize(t,"eng+chi_sim")).data.text}export{o as recognizeWithTesseract};
