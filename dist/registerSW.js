1929137e3d0fb555ad3286685bf03724fcfa2e14
orangeCheers <chengqingkai@ysten.com>
2026-04-20 17:05:35 +0800

    init

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/smart-study/sw.js', { scope: '/smart-study/' })})}
